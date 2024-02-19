8437d0d99c379e99ca81c312c7be63998ed1ceff
Kgomotso222 <kgomotsonthuping22@gmail.com>
2024-02-19 05:03:41 -0800

    model

--- models/user.py
+++ models/user.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-
+import os
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+   __tablename__ = 'users'
     email = Column(
         String(128),
         nullable=False
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
     password = Column(
         String(128),
         nullable=False
-    )
-    irst_name = Column(
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
+    first_name = Column(
         String(128),
         nullable=True
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
     last_name = Column(
         String(128),
         nullable=True
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''

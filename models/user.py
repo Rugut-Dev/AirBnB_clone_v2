e3a64dc16af1bacad4833c3ebd424814b12fcfb6
Kgomotso222 <kgomotsonthuping22@gmail.com>
2024-02-19 05:05:18 -0800

    model

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
